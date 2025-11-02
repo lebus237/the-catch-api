3aaaa7baf0333ac43050aa340d046b8721bad06c
elkira01 <elkira01@gmail.com>
2025-11-02 21:47:04 +0100

    federation model

--- app/shared/infrastructure/repository/active_records_manager.ts
+++ app/shared/infrastructure/repository/active_records_manager.ts
@@ -1,5 +1,5 @@
 export class ActiveRecordManager {
-  async getActiveRecord(EntityClass: new () => { constructor: any }) {
+  async getActiveRecord(EntityClass: any) {
     const instance = new EntityClass()
     const ar = () =>
       import('#database/active-records/' + instance.constructor.name + '_model').then(
